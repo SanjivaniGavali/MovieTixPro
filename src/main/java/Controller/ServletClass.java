0c517f993cb6145c5dbb25580bc9355cc81aec27
SanjivaniGavali <sanjivanigavali999@gmail.com>
2023-07-22 15:36:42 +0530

    inital commit

--- src/main/java/Controller/ServletClass.java
+++ src/main/java/Controller/ServletClass.java
@@ -16,15 +16,12 @@ public class ServletClass extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String name=req.getParameter("name");
 		int ticket=Integer.parseInt(req.getParameter("ticket"));
-		
 		DaoClass d1=new DaoClass();
-		
 		double price=d1.fetchprice(name);
 		int totalTicket=d1.fetchticket(name);
 		int remainingTicket=d1.remainingTicket(ticket, totalTicket);
 		d1.update(remainingTicket, name);
-		double bill=d1.bill(price, ticket,remainingTicket);
-		
+		double bill=d1.bill(price, ticket,remainingTicket);	
 		req.setAttribute("name", name);
 		req.setAttribute("ticket", ticket);
 		req.setAttribute("remaining", remainingTicket);
